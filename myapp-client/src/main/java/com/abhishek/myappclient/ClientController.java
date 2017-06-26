089ca737f539593f9f7c654d8740efe39317793a
abhishekjawali <abhishek.jawali@emc.com>
2017-06-26 13:51:01 +0530

    included hystrix

--- myapp-client/src/main/java/com/abhishek/myappclient/ClientController.java
+++ myapp-client/src/main/java/com/abhishek/myappclient/ClientController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by jawala on 6/26/2017.
@@ -14,11 +13,13 @@ import org.springframework.web.client.RestTemplate;
 public class ClientController {
 
     @Autowired
-    private RestTemplate restTemplate;
+    private ClientService clientService;
+
 
     @GetMapping
     public String hello() {
-        String url = "http://MYAPP-SERVER/api/server";
-        return restTemplate.getForObject(url, String.class);
+        return clientService.getHelloServer();
     }
+
+   
 }
